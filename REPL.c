d351819bae85655a4b62e1c649393f0f17a23448
Guilherme <trintaeoitogc@gmail.com>
2020-12-04 00:09:08 -0300

    feat: created function that checks what type of command (insert, select ...) is inserted in the shell and returns PREPARE_SUCCESS or PREPARE_UNRECOGNIZED_STATEMENT

--- REPL.c
+++ REPL.c
@@ -67,6 +67,19 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 	}
 }
 
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
+	if(strncmp(input_buffer->buffer, "insert", 6) == 0) {
+		statement->type = STATEMENT_INSERT;
+		return PREPARE_SUCCESS;	
+	}
+	if (strcmp(input_buffer->buffer, "select") == 0) {
+		statement->type = STATEMENT_SELECT;
+		return PREPARE_SUCCESS;	
+	}
+
+	return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
 int main(int argc, char* argv[]) {
 	InputBuffer* input_buffer = new_input_buffer();
 
