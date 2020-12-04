819b52e64e38a5e0a5a7aab697a2648e5614925f
Guilherme <trintaeoitogc@gmail.com>
2020-12-03 23:02:54 -0300

    feat: built the REPL code

---
+++ REPL.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include <string.h>
+
+typedef struct {
+	char* 	buffer;
+	size_t 	buffer_length;
+	ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+	InputBuffer* input_buffer 	= (InputBuffer*)malloc(sizeof(InputBuffer));
+	input_buffer->buffer 		= NULL;
+	input_buffer->buffer_length = 0;
+	input_buffer->input_length 	= 0;
+
+	return input_buffer;
+}
+
+void print_prompt() {
+	printf("db> ");
+}
+
+void read_input(InputBuffer* input_buffer){
+	ssize_t bytes_read = getline( &(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+	if (bytes_read <= 0) {
+		printf("Error reading input \n");
+		exit(EXIT_FAILURE);
+	}
+
+	input_buffer->input_length = bytes_read - 1;
+	input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+	free(input_buffer->buffer);
+	free(input_buffer);
+}
+
+int main(int argc, char* argv[]) {
+	InputBuffer* input_buffer = new_input_buffer();
+
+	while(1) {
+		print_prompt();
+		read_input(input_buffer);
+
+		if(strcmp(input_buffer->buffer, ".exit") == 0) {
+			close_input_buffer(input_buffer);
+			exit(EXIT_SUCCESS);
+		}
+		else {
+			printf("Unrecognized command '%s .\n'", input_buffer->buffer);
+		}
+	}
+}
