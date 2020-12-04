43664cf125e838fea9f75d21ae98ae61426b106e
Guilherme <trintaeoitogc@gmail.com>
2020-12-04 00:01:25 -0300

    feat: created the ENUM that stores command types (like a reserved word) and also created an enum that for now only stores the Statement type

--- REPL.c
+++ REPL.c
@@ -50,6 +50,15 @@ typedef enum {
 	PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
+typedef enum {
+	STATEMENT_INSERT, 
+	STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+	StatementType type;
+} Statement;
+
 MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 	if(strcmp(input_buffer->buffer, ".exit") == 0){
 		exit(EXIT_SUCCESS);	
