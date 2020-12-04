622607f17f4659e14cece72cde217163f4944e5d
Guilherme <trintaeoitogc@gmail.com>
2020-12-03 23:48:01 -0300

    feat: made checks whether the command is a meta-command or a sql command, in addition to added switch cases with the results of enums that are created in the next commit

--- REPL.c
+++ REPL.c
@@ -40,6 +40,7 @@ void close_input_buffer(InputBuffer* input_buffer) {
 	free(input_buffer);
 }
 
+
 int main(int argc, char* argv[]) {
 	InputBuffer* input_buffer = new_input_buffer();
 
@@ -47,12 +48,26 @@ int main(int argc, char* argv[]) {
 		print_prompt();
 		read_input(input_buffer);
 
-		if(strcmp(input_buffer->buffer, ".exit") == 0) {
-			close_input_buffer(input_buffer);
-			exit(EXIT_SUCCESS);
+		if(input_buffer->buffer[0] == '.') {
+			switch (do_meta_command(input_buffer)) {
+				case (META_COMMAND_SUCCESS):
+					continue;
+				case (META_COMMAND_UNRECOGNIZED_COMMAND):
+					printf("Unrecognized command '%s'\n", input_buffer);
+					continue;
+			}
+		}	
+
+		Statement statement;
+		switch(prepare_statement(input_buffer, &statement)) {
+			case (PREPARE_SUCCESS):	
+				break;
+			case (PREPARE_UNRECOGNIZED_STATEMENT):
+				printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+				continue;
 		}
-		else {
-			printf("Unrecognized command '%s .\n'", input_buffer->buffer);
-		}
+
+		execute_statement(&statement);
+		printf("Executed.\n");
 	}
 }
