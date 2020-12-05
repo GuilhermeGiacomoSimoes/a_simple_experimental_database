da5b39103950af834866b90c4750566075111668
Guilherme <trintaeoitogc@gmail.com>
2020-12-04 22:38:23 -0300

    fix: Code organization

--- REPL.c
+++ REPL.c
@@ -4,6 +4,25 @@
 #include<stdbool.h>
 #include<string.h>
 
+typedef enum {
+	META_COMMAND_SUCCESS,
+	META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+	PREPARE_SUCCESS,
+	PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum {
+	STATEMENT_INSERT, 
+	STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+	StatementType type;
+} Statement;
+
 typedef struct {
 	char* 	buffer;
 	size_t 	buffer_length;
@@ -39,25 +58,6 @@ void close_input_buffer(InputBuffer* input_buffer) {
 	free(input_buffer->buffer);
 	free(input_buffer);
 }
-
-typedef enum {
-	META_COMMAND_SUCCESS,
-	META_COMMAND_UNRECOGNIZED_COMMAND
-} MetaCommandResult;
-
-typedef enum {
-	PREPARE_SUCCESS,
-	PREPARE_UNRECOGNIZED_STATEMENT
-} PrepareResult;
-
-typedef enum {
-	STATEMENT_INSERT, 
-	STATEMENT_SELECT
-} StatementType;
-
-typedef struct {
-	StatementType type;
-} Statement;
 
 MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 	if(strcmp(input_buffer->buffer, ".exit") == 0){
