2e3e026b222835d8b3061f9da16f90ebaedc3f3e
Guilherme <trintaeoitogc@gmail.com>
2020-12-04 22:56:10 -0300

    feat: captured parameters after the reserved word insert and stored in the statement's row_to_insert

--- REPL.c
+++ REPL.c
@@ -3,6 +3,16 @@
 #include<stdlib.h>
 #include<stdbool.h>
 #include<string.h>
+#include<stdint.h>
+
+#define  COLUMN_USERNAME_SIZE 32
+#define  COLUMN_EMAIL_SIZE    255 
+
+typedef struct {
+	uint32_t id;
+	char username[COLUMN_USERNAME_SIZE];
+	char email[COLUMN_EMAIL_SIZE];
+} Row;
 
 typedef enum {
 	META_COMMAND_SUCCESS,
@@ -21,6 +31,7 @@ typedef enum {
 
 typedef struct {
 	StatementType type;
+	Row row_to_insert;
 } Statement;
 
 typedef struct {
@@ -70,6 +81,13 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
 	if(strncmp(input_buffer->buffer, "insert", 6) == 0) {
 		statement->type = STATEMENT_INSERT;
+
+		int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
+
+		if(args_assigned < 3){
+			return PREPARE_SYNTAX_ERROR;
+		} 
+
 		return PREPARE_SUCCESS;	
 	}
 	if (strcmp(input_buffer->buffer, "select") == 0) {
