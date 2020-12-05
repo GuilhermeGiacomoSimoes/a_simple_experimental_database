5a9066c483193934b3839afdf4e6cf96a56c8835
Guilherme <trintaeoitogc@gmail.com>
2020-12-04 23:37:33 -0300

    feat: created variables for serializing a line

--- REPL.c
+++ REPL.c
@@ -7,12 +7,24 @@
 
 #define  COLUMN_USERNAME_SIZE 32
 #define  COLUMN_EMAIL_SIZE    255 
+#define  size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 
 typedef struct {
 	uint32_t id;
 	char username[COLUMN_USERNAME_SIZE];
 	char email[COLUMN_EMAIL_SIZE];
 } Row;
+
+const uint32_t ID_SIZE 			= size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE 	= size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE 		= size_of_attribute(Row, email);
+
+const uint32_t ID_OFFSET 		= 0; 
+const uint32_t USERNAME_OFFSET 	= ID_OFFSET + ID_SIZE; 
+const uint32_t EMAIL_OFFSET 	= USERNAME_OFFSET + USERNAME_SIZE; 
+const uint32_t ROW_SIZE 		= EMAIL_OFFSET + EMAIL_SIZE; 
+
+
 
 typedef enum {
 	META_COMMAND_SUCCESS,
