10755424e139334ed397c796eab9379db70f544f
Guilherme <trintaeoitogc@gmail.com>
2020-12-09 21:42:08 -0300

    fix: fix problem in serialize and deserialize

--- REPL.c
+++ REPL.c
@@ -26,14 +26,14 @@ const uint32_t ROW_SIZE 		= EMAIL_OFFSET + EMAIL_SIZE;
 
 void serialize_row(Row* source, void* destination) {
 	memcpy(destination + ID_OFFSET, 		&(source->id), ID_SIZE		);
-	memcpy(destination + USERNAME_OFFSET,	&(source->id), USERNAME_SIZE);
-	memcpy(destination + EMAIL_OFFSET,		&(source->id), EMAIL_OFFSET	);
+	memcpy(destination + USERNAME_OFFSET,	&(source->username), USERNAME_SIZE);
+	memcpy(destination + EMAIL_OFFSET,		&(source->email), EMAIL_OFFSET	);
 }
 
 void deserialize_row(void* source, Row* destination) {
-	memcpy(&(destination->id		), source + ID_OFFSET, ID_SIZE);
-	memcpy(&(destination->username	), source + ID_OFFSET, ID_SIZE);
-	memcpy(&(destination->email		), source + ID_OFFSET, ID_SIZE);
+	memcpy(&(destination->id		), source + ID_OFFSET,       ID_SIZE);
+	memcpy(&(destination->username	), source + USERNAME_OFFSET, ID_SIZE);
+	memcpy(&(destination->email		), source + EMAIL_OFFSET,    ID_SIZE);
 }
 
 
