d4520876009ba9b48fb6b4016a7af48c944f25ca
Guilherme <trintaeoitogc@gmail.com>
2020-12-09 21:45:06 -0300

    feat: a Table structure that points to pages of rows and keeps track of how many rows there are

--- REPL.c
+++ REPL.c
@@ -8,12 +8,15 @@
 #define  COLUMN_USERNAME_SIZE 32
 #define  COLUMN_EMAIL_SIZE    255 
 #define  size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+#define  TABLE_MAX_PAGES 100 
 
 typedef struct {
 	uint32_t id;
 	char username[COLUMN_USERNAME_SIZE];
 	char email[COLUMN_EMAIL_SIZE];
 } Row;
+
+const uint32_t PAGE_SIZE        = 4096;
 
 const uint32_t ID_SIZE 			= size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE 	= size_of_attribute(Row, username);
@@ -23,6 +26,14 @@ const uint32_t ID_OFFSET 		= 0;
 const uint32_t USERNAME_OFFSET 	= ID_OFFSET + ID_SIZE; 
 const uint32_t EMAIL_OFFSET 	= USERNAME_OFFSET + USERNAME_SIZE; 
 const uint32_t ROW_SIZE 		= EMAIL_OFFSET + EMAIL_SIZE; 
+
+const uint32_t ROWS_PER_PAGE  = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct {
+	uint32_t num_rows;
+	void* pages[TABLE_MAX_PAGES];
+} Table; 
 
 void serialize_row(Row* source, void* destination) {
 	memcpy(destination + ID_OFFSET, 		&(source->id), ID_SIZE		);
