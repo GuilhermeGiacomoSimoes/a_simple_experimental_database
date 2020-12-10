a05ea02c2bb795a3dc8268f3570b8c35f1150fa5
Guilherme <trintaeoitogc@gmail.com>
2020-12-09 22:00:19 -0300

    feat: here is how we figure out where to read/write in memory for a particular row

--- REPL.c
+++ REPL.c
@@ -36,9 +36,9 @@ typedef struct {
 } Table; 
 
 void serialize_row(Row* source, void* destination) {
-	memcpy(destination + ID_OFFSET, 		&(source->id), ID_SIZE		);
+	memcpy(destination + ID_OFFSET, 		&(source->id), 		 ID_SIZE      );
 	memcpy(destination + USERNAME_OFFSET,	&(source->username), USERNAME_SIZE);
-	memcpy(destination + EMAIL_OFFSET,		&(source->email), EMAIL_OFFSET	);
+	memcpy(destination + EMAIL_OFFSET,		&(source->email),    EMAIL_OFFSET );
 }
 
 void deserialize_row(void* source, Row* destination) {
@@ -47,6 +47,19 @@ void deserialize_row(void* source, Row* destination) {
 	memcpy(&(destination->email		), source + EMAIL_OFFSET,    ID_SIZE);
 }
 
+void* row_slot(Table* table, uint32_t row_num) {
+	uint32_t page_num = row_num / ROWS_PER_PAGE;
+	void* page = table->pages[page_num];
+
+	if (page == NULL) {
+		page = table->pages[page_num] = malloc(PAGE_SIZE);	
+	}
+
+	uint32_t row_offset  = row_num %ROWS_PER_PAGE;
+	uint32_t byte_offset = row_offset * ROW_SIZE; 
+
+	return page + byte_offset;
+}
 
 typedef enum {
 	META_COMMAND_SUCCESS,
