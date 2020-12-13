83e991c4f85475f90e4906d45e637ce5d173682a
Guilherme <trintaeoitogc@gmail.com>
2020-12-13 17:16:52 -0300

    feat: cmake adding to project

---
+++ CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/src/REPL.o"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
