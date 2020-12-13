5a07aaa5d41ccbc69adef6d7d0bd05ce461b079d
Guilherme <trintaeoitogc@gmail.com>
2020-12-13 15:41:41 -0300

    feat: creating tests diretory and writing ruby file from REPL test

---
+++ tests/tests.rb
@@ -0,0 +1,30 @@
+describe 'database' do
+  def run_script(commands)
+    raw_output = nil
+    IO.popen("../src/REPL.c", "r+") do |pipe|
+      commands.each do |commands|
+        pipe.puts command
+      end
+
+      pipe.close_write
+
+      raw_output = pipe.gets(nil)
+    end
+    raw_output.split("\n")
+  end
+
+  it 'inserts and retrieves a row' do
+    result = run_script([
+      "insert 1 user1 person1@example.com",
+      "select",
+      ".exit"
+    ])
+
+    expect(result).to match_array([
+      "db> Executed.",
+      "db> (1, user1, person1@example.com)",
+      "Executed",
+      "db>"
+    ])
+  end
+end
