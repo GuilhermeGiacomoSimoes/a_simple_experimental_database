9ea0dfe61f0be4d2fb70f118d95537c567b6cece
simoes <guilherme.simoes@kabum.com.br>
2021-04-15 13:25:20 -0300

    fix: fix functions of the serialize and deserialize row

--- REPL.c
+++ REPL.c
@@ -36,15 +36,15 @@ typedef struct {
 } Table; 
 
 void serialize_row(Row* source, void* destination) {
-	memcpy(destination + ID_OFFSET, 		&(source->id), 		 ID_SIZE      );
-	memcpy(destination + USERNAME_OFFSET,	&(source->username), USERNAME_SIZE);
-	memcpy(destination + EMAIL_OFFSET,		&(source->email),    EMAIL_OFFSET );
+  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
 }
 
-void deserialize_row(void* source, Row* destination) {
-	memcpy(&(destination->id		), source + ID_OFFSET,       ID_SIZE);
-	memcpy(&(destination->username	), source + USERNAME_OFFSET, ID_SIZE);
-	memcpy(&(destination->email		), source + EMAIL_OFFSET,    ID_SIZE);
+void deserialize_row(void *source, Row* destination) {
+  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
 void* row_slot(Table* table, uint32_t row_num) {
