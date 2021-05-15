764a2556cdc4ecae224a0acd6d2a5f504f22b1b9
Guilherme <trintaeoitogc@gmail.com>
2021-05-15 12:16:15 -0300

    feat: from now on, prepare_statement returns the value of prepare_insert

--- REPL.c
+++ REPL.c
@@ -149,15 +149,7 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table *table) {
 
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
 	if(strncmp(input_buffer->buffer, "insert", 6) == 0) {
-		statement->type = STATEMENT_INSERT;
-
-		int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
-
-		if(args_assigned != 3){
-			return PREPARE_SYNTAX_ERROR;
-		} 
-
-		return PREPARE_SUCCESS;	
+		return prepare_insert(input_buffer, statement);	
 	}
 	if (strcmp(input_buffer->buffer, "select") == 0) {
 		statement->type = STATEMENT_SELECT;
