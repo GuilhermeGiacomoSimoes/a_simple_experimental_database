c16e1f945ccaf76bb7422c685c7035104d6790a3
Guilherme <trintaeoitogc@gmail.com>
2021-05-16 10:42:39 -0300

    feat: from now on, instead creating the pages from malloc, is calling function get_page

--- REPL.c
+++ REPL.c
@@ -132,11 +132,8 @@ void db_close(Table* table) {
 
 void* row_slot(Table* table, uint32_t row_num) {
 	uint32_t page_num = row_num / ROWS_PER_PAGE;
-	void* page = table->pages[page_num];
 
-	if (page == NULL) {
-		page = table->pages[page_num] = malloc(PAGE_SIZE);	
-	}
+	void* page = get_page(table->pager, page_num);
 
 	uint32_t row_offset  = row_num    % ROWS_PER_PAGE;
 	uint32_t byte_offset = row_offset * ROW_SIZE; 
