af5497b805498cbad567924955622438615fd0cc
Guilherme <trintaeoitogc@gmail.com>
2021-05-16 10:43:22 -0300

    feat: creating function from memory free

--- REPL.c
+++ REPL.c
@@ -209,11 +209,25 @@ void close_input_buffer(InputBuffer* input_buffer) {
 	free(input_buffer);
 }
 
-void free_table(Table* table) {
-	for (int i = 0; table->pages[i]; i++) {
-		free(table->pages[i]);	
+void pager_flush(Pager* pager, uint32_t page_num, uint32_t size) {
+	if(pager->pages[page_num] == NULL) {
+		printf("Tried to flush null page \n");
+		exit(EXIT_FAILURE);
 	}
-	free(table);
+
+	off_t offset = lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+
+	if(offset == -1) {
+		printf("Error seeking: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+
+	ssize_t bytes_written = write(pager->file_descriptor, pager->pages[page_num], size);
+
+	if(bytes_written == -1){
+		printf("Error writing: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
 }
 
 MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table *table) {
