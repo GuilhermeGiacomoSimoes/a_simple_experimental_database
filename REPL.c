9b21906c8f4b9da35dbc2f403832c391887dd750
Guilherme <trintaeoitogc@gmail.com>
2021-05-16 10:41:04 -0300

    feat: creating functions get_page and db_close

--- REPL.c
+++ REPL.c
@@ -58,6 +58,78 @@ void deserialize_row(void *source, Row* destination) {
   memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+void* get_page(Pager* pager, uint32_t page_num) {
+	if(page_num > TABLE_MAX_PAGES) {
+		printf("Tried to fetch page number out of bounds %d > %d\n", page_num, TABLE_MAX_PAGES);
+		exit(EXIT_FAILURE);
+	}
+
+	if(pager->pages[page_num] == NULL) {
+		void* page = malloc(PAGE_SIZE);
+		uint32_t num_pages = pager->file_length / PAGE_SIZE;
+
+		if(pager->file_length % PAGE_SIZE) {
+			num_pages += 1;
+		}
+		
+		if(page_num <= num_pages) {
+			lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+			ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+
+			if(bytes_read == -1) {
+				printf("Error reading gile %d\n", errno); 
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		pager->pages[page_num] = page;
+	}
+
+	return pager->pages[page_num];
+}
+
+void db_close(Table* table) {
+	Pager* pager = table->pager;
+	uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
+
+	for(uint32_t i = 0; i < num_full_pages; i++) {
+		if(pager->pages[i] == NULL) {
+			continue;
+		}
+		pager_flush(pager, i, PAGE_SIZE);
+		free(pager->pages[i]);
+		pager->pages[i] = NULL;
+	}
+
+	uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+	if(num_additional_rows > 0) {
+		uint32_t page_num = num_full_pages;
+
+		if(pager->pages[page_num] != NULL) {
+			pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+			free(pager->pages[page_num]);
+			pager->pages[page_num] = NULL; 
+		}
+	}
+
+	int result = close(pager->file_descriptor);
+	if(result == -1) {
+		printf("Error closing do file. \n");
+		exit(EXIT_FAILURE);
+	}
+
+	for(uint32_t i = 0; i< TABLE_MAX_PAGES; i++) {
+		void* page = pager->pages[i];
+		if(page) {
+			free(page);
+			pager->pages[i] = NULL;
+		}
+	}
+
+	free(pager);
+	free(table);
+}
+
 void* row_slot(Table* table, uint32_t row_num) {
 	uint32_t page_num = row_num / ROWS_PER_PAGE;
 	void* page = table->pages[page_num];
