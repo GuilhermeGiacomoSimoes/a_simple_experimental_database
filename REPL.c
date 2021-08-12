2bb89acbe99be32d5da511a0d6511ea6ac00701b
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-08-11 23:11:01 -0300

    chore: identify a position by the page number of the node, and the cell number within that node.

--- REPL.c
+++ REPL.c
@@ -86,15 +86,20 @@ typedef struct {
 
 typedef struct { 
 	Table* table;
-	uint32_t row_num;
+	uint32_t page_num;
+	uint32_t cell_num;
 	bool end_of_table;
 } Cursor;
 
 Cursor* table_start(Table* table){
 	Cursor* cursor = ( Cursor* ) malloc(sizeof(Cursor));
 	cursor->table = table;
-	cursor->row_num = 0;
-	cursor->end_of_table = ( table->num_rows == 0 );
+	cursor->page_num = table->root_page_num;
+	cursor->cell_num = 0;
+
+	void* root_node = get_page(table->pager, table->root_page_num);
+	uint32_t num_cells = *leaf_node_num_cells(root_node);
+	cursor->end_of_table = (num_cells == 0);
 
 	return cursor;
 }
@@ -102,7 +107,11 @@ Cursor* table_start(Table* table){
 Cursor* table_end( Table* table ) {
 	Cursor* cursor = ( Cursor* ) malloc(sizeof(Cursor));
 	cursor->table = table;
-	cursor->row_num = table->num_rows;
+	cursor->page_num = table->root_page_num;
+
+	void* root_node = get_page(table->pager, table->root_page_num); 
+	uint32_t num_cells = *leaf_node_num_cells(root_node);
+	cursor->cell_num = num_cells;
 	cursor->end_of_table = true;
 
 	return cursor;
@@ -207,15 +216,10 @@ void db_close(Table* table) {
 }
 
 void* cursor_value(Cursor* cursor) {
-	uint32_t row_num = cursor->row_num;
-	uint32_t page_num = row_num / ROWS_PER_PAGE;
-
+	uint32_t page_num = cursor->page_num;
 	void* page = get_page(cursor->table->pager, page_num);
 
-	uint32_t row_offset  = row_num    % ROWS_PER_PAGE;
-	uint32_t byte_offset = row_offset * ROW_SIZE; 
-
-	return page + byte_offset;
+	return leaf_node_value(page, cursor->cell_num);
 }
 
 void print_row(Row* row) {
@@ -357,8 +361,11 @@ ExecuteResult execute_insert(Statement* statement, Table* table) {
 }
 
 void cursor_advance( Cursor* cursor ) {
-	cursor->row_num += 1;
-	if ( cursor->row_num >= cursor->table->num_rows ) {
+	uint32_t page_num = cursor->page_num;
+	void* node = get_page(cursor->table->pager, page_num);
+
+	cursor->cell_num += 1;
+	if(cursor->cell_num >= (*leaf_node_num_cells(node))){
 		cursor->end_of_table = true;
 	}
 }
