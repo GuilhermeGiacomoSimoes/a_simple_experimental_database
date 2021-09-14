5d42ef7d7bb4948c8f8cad263d5a3dc1ba71ec0c
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-09-13 22:51:08 -0300

    refactor: refactor of the prepare_insert

--- REPL.c
+++ REPL.c
@@ -290,45 +290,74 @@ void close_input_buffer(InputBuffer* input_buffer) {
 	free(input_buffer);
 }
 
+void print_constant() {
+	printf("ROW_SIZE: %d\n", ROW_SIZE);
+	printf("LEAF_NODE_HEADER_SIZE: %d\n", LEAF_NODE_HEADER_SIZE);
+	printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_NODE_HEADER_SIZE);
+	printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
+	printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
+	printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+}
+
+void print_leaf_node(void* node) {
+	uint32_t num_cells = *leaf_node_num_cells(node);
+	printf("leaf (size %d)\n", num_cells);
+	for(uint32_t i=0; i<num_cells; i++) {
+		uint32_t key = *leaf_node_key(node, i);
+		printf("   - %d : %d\n", i, key);
+	}
+}
+
 
 MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table *table) {
 	if(strcmp(input_buffer->buffer, ".exit") == 0){
 		db_close(table); 
 		exit(EXIT_SUCCESS);	
-	}else {
+	} 
+	else if(strcmp(input_buffer->buffer, ".btree") == 0) {
+		printf("Tree:\n");
+		print_leaf_node(get_page(table->pager, 0));
+		return META_COMMAND_SUCCESS
+	}
+	else if(strcmp(input_buffer->buffer, ".constants") == 0) {
+		printf("Constants: \n");
+		print_constant();
+		return META_COMMAND_SUCCESS;
+	}
+	else {
 		return META_COMMAND_UNRECOGNIZED_COMMAND;	
 	}
 }
 
 PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
-  statement->type = STATEMENT_INSERT;
+	statement->type = STATEMENT_INSERT;
 
-  char* keyword = strtok(input_buffer->buffer, " ");
-  char* id_string = strtok(NULL, " ");
-  char* username = strtok(NULL, " ");
-  char* email = strtok(NULL, " ");
+	char* keyword = strtok(input_buffer->buffer, " ");
+	char* id_string = strtok(NULL, " ");
+	char* username = strtok(NULL, " ");
+	char* email = strtok(NULL, " ");
 
-  if (id_string == NULL || username == NULL || email == NULL) {
-    return PREPARE_SYNTAX_ERROR;
-  }
+	if (id_string == NULL || username == NULL || email == NULL) {
+		return PREPARE_SYNTAX_ERROR;
+	}
 
-  int id = atoi(id_string);
-  if( id < 0) {
-  	return PREPARE_NEGATIVE_ID; 
-  }
+	int id = atoi(id_string);
+	if( id < 0) {
+		return PREPARE_NEGATIVE_ID; 
+	}
 
-  if (strlen(username) > COLUMN_USERNAME_SIZE) {
-    return PREPARE_STRING_TOO_LONG;
-  }
-  if (strlen(email) > COLUMN_EMAIL_SIZE) {
-    return PREPARE_STRING_TOO_LONG;
-  }
+	if (strlen(username) > COLUMN_USERNAME_SIZE) {
+		return PREPARE_STRING_TOO_LONG;
+	}
+	if (strlen(email) > COLUMN_EMAIL_SIZE) {
+		return PREPARE_STRING_TOO_LONG;
+	}
 
-  statement->row_to_insert.id = id;
-  strcpy(statement->row_to_insert.username, username);
-  strcpy(statement->row_to_insert.email, email);
+	statement->row_to_insert.id = id;
+	strcpy(statement->row_to_insert.username, username);
+	strcpy(statement->row_to_insert.email, email);
 
-  return PREPARE_SUCCESS;
+	return PREPARE_SUCCESS;
 }
 
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
