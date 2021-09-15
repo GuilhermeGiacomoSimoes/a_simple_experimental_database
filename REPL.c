3dca22dccd3c8eb0bb4d303283c6ffd5a368465b
Guilherme Giácomo Simões <simoes@Latitude-E7450-8c8ac6a2.localdomain>
2021-09-14 23:06:52 -0300

    fix: the line did not contain a semicolon

--- REPL.c
+++ REPL.c
@@ -317,7 +317,7 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table *table) {
 	else if(strcmp(input_buffer->buffer, ".btree") == 0) {
 		printf("Tree:\n");
 		print_leaf_node(get_page(table->pager, 0));
-		return META_COMMAND_SUCCESS
+		return META_COMMAND_SUCCESS; 
 	}
 	else if(strcmp(input_buffer->buffer, ".constants") == 0) {
 		printf("Constants: \n");
