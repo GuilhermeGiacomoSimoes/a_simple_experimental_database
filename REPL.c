34a6f24b34cde5f20ed1ea2a355fe062d216e38b
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2021-09-15 22:53:53 -0300

    feature: implements function table_find

--- REPL.c
+++ REPL.c
@@ -359,6 +359,19 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 	return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
+Cursor* table_find(Table* table, uint32_t key) {
+	uint32_t root_page_num = table->root_page_num;
+	void* root_node = get_page(table->pager, root_page_num);
+
+	if(get_node_type(root_node) == NODE_LEAF){
+		return leaf_node_find(table,root_page_num,key);
+	}
+	else {
+		printf("Need to implement searching an internal node\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
 ExecuteResult execute_insert(Statement* statement, Table* table) {
 	void* node = get_page(table->pager, table->root_page_num);
 	uint32_t num_cells = *leaf_node_num_cells(node);
