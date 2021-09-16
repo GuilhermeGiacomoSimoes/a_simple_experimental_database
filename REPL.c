46c5fc21c8cc32c79a5d6da30a294e773f8f4898
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2021-09-15 23:00:26 -0300

    feat: This will either return the position of the key, the position of another key that well need to move if we want to insert the new key or the position one past the last key

--- REPL.c
+++ REPL.c
@@ -359,6 +359,36 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 	return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
+Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key) {
+	void* node = get_page(table->pager, page_num);
+	uint32_t num_cells = *leaf_node_num_cells(node);
+
+	Cursor* cursor = malloc(sizeof(Cursor));
+	cursor->table = table;
+	cursor->page_num = page_num;
+
+	uint32_t min_index = 0;
+	uint32_t one_past_max_index = num_cells;
+	while(one_past_max_index != min_index) {
+		uint32_t index = (min_index + one_past_max_index) / 2;
+		uint32_t key_at_index = *leaf_node_key(node, index);
+
+		if( key == key_at_index ) {
+			cursor->cell_num = index;
+			return cursor;
+		}
+
+		if(key < key_at_index) {
+			one_past_max_index = index;
+		} else {
+			min_index = index + 1;
+		}
+	}
+
+	cursor->cell_num = min_index;
+	return cursor;
+}
+
 Cursor* table_find(Table* table, uint32_t key) {
 	uint32_t root_page_num = table->root_page_num;
 	void* root_node = get_page(table->pager, root_page_num);
