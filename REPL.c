7353da34d6518849cb76893cc8cc9e661b702829
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2021-09-15 22:45:23 -0300

    refactor: table_end dont necessary

--- REPL.c
+++ REPL.c
@@ -138,19 +138,6 @@ Cursor* table_start(Table* table){
 	return cursor;
 }
 
-Cursor* table_end( Table* table ) {
-	Cursor* cursor = ( Cursor* ) malloc(sizeof(Cursor));
-	cursor->table = table;
-	cursor->page_num = table->root_page_num;
-
-	void* root_node = get_page(table->pager, table->root_page_num); 
-	uint32_t num_cells = *leaf_node_num_cells(root_node);
-	cursor->cell_num = num_cells;
-	cursor->end_of_table = true;
-
-	return cursor;
-}
-
 //void * memcpy ( void * destination, const void * source, size_t num );
 void serialize_row(Row* source, void* destination) {
 	memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
