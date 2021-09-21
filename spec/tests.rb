61e29803fe9e9761d9a86df08073d527eda6d6df
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2021-09-20 22:37:47 -0300

    fix: LEAF_NODE_CELL_SIZE is 297

--- spec/tests.rb
+++ spec/tests.rb
@@ -88,8 +88,9 @@ describe 'database' do
       "db> Constants: ",
       "ROW_SIZE: 293", 
       "COMMON_NODE_HEADER_SIZE: 6",
-      "LEAF_NODE_CELL_SIZE: 10",
+      "LEAF_NODE_CELL_SIZE: 297",
       "LEAF_NODE_SPACE_FOR_CELLS: 4086",
+      "LEAF_NODE_HEADER_SIZE: 10",
       "LEAF_NODE_MAX_CELLS: 13",
       "db> ",
     ])
