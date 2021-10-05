046dcdd9bbb1d12748215d19f0967a56514d9ac3
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2021-10-04 22:43:03 -0300

    feat: create functions for reading and writing to an internal node

--- REPL.c
+++ REPL.c
@@ -104,6 +104,50 @@ void initialize_leaf_node(void* node) {
 	*leaf_node_num_cells(node) = 0;
 }
 
+uint32_t* internal_node_num_keys(void* node){
+	return node + INTERNAL_NODE_NUM_KEYS_OFFSET;
+}
+
+void initialize_internal_node(void* node) {
+	set_node_type(node, NODE_INTERNAL);
+	set_node_root(node, false);
+	*internal_node_num_keys(node) = 0;
+}
+
+uint32_t* internal_node_right_child(void* node){
+	return node + INTERNAL_NODE_RIGHT_CHILD_OFFSET;
+}
+
+uint32_t* internal_node_child(void* node, uint32_t child_num) {
+	uint32_t num_keys = *internal_node_num_keys(node);
+	if(child_num > num_keys) {
+		printf("Tried to access child_num %d > num_keys %d\n", child_num, num_keys);
+		exit(EXIT_FAILURE);
+	} else if (child_num == num_keys) {
+		return internal_node_right_child(node);
+	} else {
+		return internal_node_cell(node, child_num);
+	}
+}
+
+uint32_t* internal_node_key(void* node, uint32_t key_num) {
+	return internal_node_cell(node, key_num) + INTERNAL_NODE_CHILD_SIZE;
+}
+
+NodeType get_node_type(void* node){
+	uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+	return (NodeType)value;
+}
+
+uint32_t get_node_max_key(void* node) {
+	switch(get_node_type(node)) {
+		case NODE_INTERNAL:
+			return *internal_node_key(node, *internal_node_num_keys(node) - 1);
+		case NODE_LEAF:
+			return *leaf_node_key(node, *leaf_node_num_cells(node) - 1);
+	}
+}
+
 typedef struct {
 	Pager* pager;
 	uint32_t root_page_num;
