276c5e9ccafc0602f7a4d1b9df7ae29c4c26b612
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2021-12-27 22:30:33 -0300

    refactor: isolated a declaration of structs, variaveis, constants in hader file

---
+++ src/REPL.h
@@ -0,0 +1,115 @@
+#define  COLUMN_USERNAME_SIZE 	32
+#define  COLUMN_EMAIL_SIZE    	255
+#define  TABLE_MAX_PAGES 		100
+#define  size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+typedef enum { NODE_INTERNAL, NODE_LEAF } NodeType;
+
+typedef struct {
+	uint32_t id;
+	char username[COLUMN_USERNAME_SIZE + 1];
+	char email[COLUMN_EMAIL_SIZE + 1];
+} Row;
+
+typedef struct {
+	int file_descriptor;
+	uint32_t file_length;
+	void* pages[TABLE_MAX_PAGES];
+	uint32_t num_pages;
+} Pager; 
+
+typedef struct {
+	Pager* pager;
+	uint32_t root_page_num;
+} Table; 
+
+typedef struct { 
+	Table* table;
+	uint32_t page_num;
+	uint32_t cell_num;
+	bool end_of_table;
+} Cursor;
+
+typedef enum {
+	STATEMENT_INSERT, 
+	STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+	StatementType type;
+	Row row_to_insert;
+} Statement;
+
+typedef struct {
+	char* 	buffer;
+	size_t 	buffer_length;
+	ssize_t input_length;
+} InputBuffer;
+
+typedef enum {
+	EXECUTE_SUCCESS, 
+	EXECUTE_TABLE_FULL, 
+	EXECUTE_DUPLICATE_KEY
+} ExecuteResult;
+
+typedef enum {
+	META_COMMAND_SUCCESS,
+	META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+	PREPARE_SUCCESS,
+	PREPARE_UNRECOGNIZED_STATEMENT,
+	PREPARE_SYNTAX_ERROR, 
+	PREPARE_STRING_TOO_LONG, 
+	PREPARE_NEGATIVE_ID
+} PrepareResult;
+
+
+const uint32_t PAGE_SIZE    = 4096;
+
+const uint32_t ID_SIZE 		= size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE= size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE 	= size_of_attribute(Row, email);
+
+const uint32_t ID_OFFSET 		= 0; 
+const uint32_t USERNAME_OFFSET 	= ID_OFFSET + ID_SIZE; 
+const uint32_t EMAIL_OFFSET 	= USERNAME_OFFSET + USERNAME_SIZE; 
+const uint32_t ROW_SIZE 		= EMAIL_OFFSET + EMAIL_SIZE; 
+
+const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
+const uint32_t NODE_TYPE_OFFSET = 0;
+const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
+const uint32_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
+const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t) ;
+const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+const uint8_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+
+const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NUM_CELSS_OFFSET = COMMON_NODE_HEADER_SIZE; 
+const uint32_t LEAF_NODE_NEXT_LEAF_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NEXT_LEAF_OFFSET = LEAF_NODE_NUM_CELSS_OFFSET + LEAF_NODE_NUM_CELLS_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE + LEAF_NODE_NEXT_LEAF_SIZE;
+
+const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_KEY_OFFSET = 0;
+const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
+const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
+const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
+
+const uint32_t LEAF_NODE_RIGHT_SPLIT_COUNT = (LEAF_NODE_MAX_CELLS + 1)/2;
+const uint32_t LEAF_NODE_LEFT_SPLIT_COUNT = (LEAF_NODE_MAX_CELLS + 1) - LEAF_NODE_RIGHT_SPLIT_COUNT;
+
+const uint32_t INTERNAL_NODE_NUM_KEYS_SIZE = sizeof(uint32_t);
+const uint32_t INTERNAL_NODE_NUM_KEYS_OFFSET = COMMON_NODE_HEADER_SIZE;
+const uint32_t INTERNAL_NODE_RIGHT_CHILD_SIZE = sizeof(uint32_t);
+const uint32_t INTERNAL_NODE_RIGHT_CHILD_OFFSET = INTERNAL_NODE_NUM_KEYS_OFFSET + INTERNAL_NODE_NUM_KEYS_SIZE; 
+const uint32_t INTERNAL_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + INTERNAL_NODE_NUM_KEYS_SIZE + INTERNAL_NODE_RIGHT_CHILD_SIZE; 
+
+const uint32_t INTERNAL_NODE_KEY_SIZE = sizeof(uint32_t);
+const uint32_t INTERNAL_NODE_CHILD_SIZE = sizeof(uint32_t);
+const uint32_t INTERNAL_NODE_CELL_SIZE = INTERNAL_NODE_CHILD_SIZE + INTERNAL_NODE_KEY_SIZE;
+const uint32_t INTERNAL_NODE_MAX_CELLS = 3;
+
