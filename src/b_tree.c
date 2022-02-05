443b67e62c1964bef24669b77f87e07dd1e82b6c
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2022-02-05 18:38:08 -0300

    feat: creating a function from create a b_tree

--- src/b_tree.c
+++ src/b_tree.c
@@ -16,6 +16,17 @@ uint32_t b_tree_search(Page *page, uint32_t wanted_element){
 	return b_tree_search(page->filhos[i])
 }
 
+Page* b_tree_create() {
+	Page *x = malloc(sizeof(Page));
+	x->folha = 1;
+	x->n = 0;
+	return x;
+}
+
+void b_tree_split_child(Page *page, uint32_t i) {
+		
+}
+
 int main () {
 
 }
