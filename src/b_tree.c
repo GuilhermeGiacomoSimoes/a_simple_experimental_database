c551dfe6794a606a6cf81a9ad849fc133e120ea1
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2022-02-05 12:19:29 -0300

    feat: create a function to search in b_tree

--- src/b_tree.c
+++ src/b_tree.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
 #include<stdint.h>
 
+uint32_t b_tree_search(Page *page, uint32_t wanted_element){
+	uint8_t i = 1;
+	while (i <= page->elems, && wanted_element > page->info[i]) {
+		i++;	
+	}
+	if( i <= page->elems && wanted_element = page->info[i] ) {
+		return page->info[i];
+	}
+	else if(page->folha) {
+		return NULL;
+	}
+
+	return b_tree_search(page->filhos[i])
+}
+
 int main () {
 
 }
