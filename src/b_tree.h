db4b09e757bf10e8a89877fe35bb13e233ee8f7e
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2022-02-05 12:19:47 -0300

    feat: create a page struct

--- src/b_tree.h
+++ src/b_tree.h
@@ -1,1 +1,11 @@
-uint32_t b_tree_search(); 
+#define MAX_ELEMENTS 10;
+
+typedef struct {
+	int folha;
+	uint16_t ordem;
+	uint16_t elems;
+	int info[MAX_VALUES + 1];
+	struct Page *filhos [MAX_VALUES + 2];
+} Page;
+
+uint32_t b_tree_search();
