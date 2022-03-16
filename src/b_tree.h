750c92a88a6e4009a1e9360ead3029153f8e40fe
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2022-03-15 23:50:13 -0300

    it is better int instead uintX_t

--- src/b_tree.h
+++ src/b_tree.h
@@ -2,10 +2,10 @@
 
 typedef struct {
 	int folha;
-	uint16_t ordem;
-	uint16_t elems;
+	int ordem;
+	int elems;
 	int info[MAX_VALUES];
-	struct Page *filhos [MAX_VALUES + 1];
+	struct Page *childs [MAX_VALUES + 1];
 } Page;
 
 uint32_t b_tree_search(Page *page, uint32_t wanted_element);
