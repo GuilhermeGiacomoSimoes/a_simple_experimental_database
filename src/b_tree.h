58d233ba240f9e9e8eebc6348b50153bdd9a95e1
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2022-03-15 23:05:24 -0300

    fix: info storage elems values and filhos storage a childs of the node... and a number of childs is equal a MAX_ELEMENTS + 1

--- src/b_tree.h
+++ src/b_tree.h
@@ -4,8 +4,8 @@ typedef struct {
 	int folha;
 	uint16_t ordem;
 	uint16_t elems;
-	int info[MAX_VALUES + 1];
-	struct Page *filhos [MAX_VALUES + 2];
+	int info[MAX_VALUES];
+	struct Page *filhos [MAX_VALUES + 1];
 } Page;
 
 uint32_t b_tree_search();
