82fe2cf10162931f1b13a68ed68994b41e12d096
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-03-19 01:11:28 -0300

    refactor: a dont needed a print constants

--- src/REPL.c
+++ src/REPL.c
@@ -107,15 +107,6 @@ void close_input_buffer(InputBuffer* input_buffer) {
 	free(input_buffer);
 }
 
-void print_constant() {
-	printf("ROW_SIZE: %d\n", ROW_SIZE);
-	printf("LEAF_NODE_HEADER_SIZE: %d\n", LEAF_NODE_HEADER_SIZE);
-	printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_NODE_HEADER_SIZE);
-	printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
-	printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
-	printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
-}
-
 void indent(uint32_t level){
 	for(uint32_t i = 0; i < level; i++) {
 		printf("  ");
@@ -166,11 +157,6 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table *table) {
 		printf("Tree:\n");
 		print_tree(table->pager, 0, 0);
 		return META_COMMAND_SUCCESS; 
-	}
-	else if(strcmp(input_buffer->buffer, ".constants") == 0) {
-		printf("Constants: \n");
-		print_constant();
-		return META_COMMAND_SUCCESS;
 	}
 	else {
 		return META_COMMAND_UNRECOGNIZED_COMMAND;	
