e9ab2e7bd8a6e3a022657affdbf04755e9782976
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-03-20 19:23:07 -0300

    feat: create a function to read the disk

--- src/disk_operation.c
+++ src/disk_operation.c
@@ -1,2 +1,39 @@
 #include<stdio.h>
 #include<stdint.h>
+
+#define NAME_FILE_DATABASE "database.db";
+
+void disk_write() {
+
+}
+
+Page* disk_read(Page* page, int number_child) {
+	int fd = open(
+			NAME_FILE_DATABASE,
+			O_RDWR |
+			O_CREAT,
+			S_IWUSR |
+			S_IRUSR 
+		);
+
+	if(fd == -1) {
+		printf("Unable to open or create file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	off_t offset = lseek(fd, page->childs[number_child], SEEK_SET);
+	if(offset == -1) {
+		printf("Error seeking: %d\n", errno);
+	}
+
+	const size_t OFFSET_PAGE = sizeof(Page); 
+	Page *child = malloc(OFFSET_PAGE);
+	size_t bytes_read = read(fd, child, OFFSET_PAGE);
+
+	if(bytes_read == -1) {
+		printf("Error while reading file database: %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
+
+	return child;
+}
