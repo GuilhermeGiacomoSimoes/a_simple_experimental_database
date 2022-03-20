884f55db6e251979bdc12f4dd93e0519a6f44013
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-03-20 19:22:33 -0300

    refactor: from now on i save the memmory address of the disk in childs

--- src/b_tree.h
+++ src/b_tree.h
@@ -5,7 +5,7 @@ typedef struct {
 	int ordem;
 	int elems;
 	int info[MAX_VALUES];
-	struct Page *childs [MAX_VALUES + 1];
+	int childs[MAX_VALUES + 1];
 } Page;
 
 uint32_t b_tree_search(Page *page, uint32_t wanted_element);
