c3eb8c2a95590eb6be47b5abb0d5184dbf2d99bc
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-03-20 19:59:26 -0300

    feat: create a disk_write function

--- src/disk_operation.c
+++ src/disk_operation.c
@@ -3,11 +3,44 @@
 
 #define NAME_FILE_DATABASE "database.db";
 
-void disk_write() {
+void disk_write(Page* page) {
+	if(page == NULL) {
+		printf("Tried write a NULL page\n");
+		exit(EXIT_FAILURE);
+	}
 
+	int fd = open(
+			NAME_FILE_DATABASE,
+			O_RDWR | O_CREAT,
+			S_IWUSR | S_IRUSR 
+		);
+
+	if(fd == -1) {
+		printf("Unable to open of create a file database\n");
+		exit(EXIT_FAILURE);
+	}
+
+	off_t offset = lseek(fd, 0, SEEK_END);
+	if(offset == -1) {
+		printf("Error seeking\n");
+		exit(EXIT_FAILURE);
+	}
+
+	const size_t OFFSET_PAGE = sizeof(Page); 
+	ssize_t bytes_written = write(fd, page, OFFSET_PAGE);
+
+	if(bytes_written == -1) {
+		printf("Error while try write file database%d\n", errno);
+		exit(EXIT_FAILURE);
+	}
 }
 
 Page* disk_read(Page* page, int number_child) {
+	if(page == NULL) {
+		printf("Tried read a NULL page\n");
+		exit(EXIT_FAILURE);
+	}
+
 	int fd = open(
 			NAME_FILE_DATABASE,
 			O_RDWR | O_CREAT,
