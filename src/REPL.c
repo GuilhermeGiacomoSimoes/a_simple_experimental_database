a404a0153a1093cf797c8eda6f4c35f0fabfd983
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-03-22 21:56:24 -0300

    refactor: the print_tree is not possible with a bigger tree

--- src/REPL.c
+++ src/REPL.c
@@ -114,42 +114,7 @@ void indent(uint32_t level){
 	}
 }
 
-void print_tree(Pager* pager, uint32_t page_num, uint32_t indentation_level) {
-	void* node = get_page(pager, page_num);
-	uint32_t num_keys, child;
-
-	switch (get_node_type(node)){
-		case (NODE_LEAF):
-			num_keys = *leaf_node_num_cells(node);
-			indent(indentation_level);
-			printf("- leaf (size %d)\n", num_keys);
-
-			for(uint32_t i = 0; i < num_keys; i++) {
-				indent(indentation_level+1);
-				printf("- %d\n", *leaf_node_key(node, i));
-			}
-			break;
-
-		case (NODE_INTERNAL):
-			num_keys = *internal_node_num_keys(node);
-			indent(indentation_level);
-			printf("- internal ( size %d )\n", num_keys);
-
-			for(uint32_t i = 0; i < num_keys; i++) {
-				child = *internal_node_child(node, i);
-				print_tree(pager, child, indentation_level + 1);
-
-				indent(indentation_level + 1);
-				printf("- key %d\n", *internal_node_key(node, i));
-			}
-
-			child = *internal_node_right_child(node);
-			print_tree(pager, child, indentation_level + 1);
-			break;
-	}
-}
-
-MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table *table) {
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Page *page) {
 	if(strcmp(input_buffer->buffer, ".exit") == 0){
 		db_close(table); 
 		exit(EXIT_SUCCESS);	
