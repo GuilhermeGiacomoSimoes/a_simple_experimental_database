f67708c0d0512ded3fdcf0c7e11c31486f684269
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-03-22 21:59:18 -0300

    chore: i wont show the tree anymore

--- src/REPL.c
+++ src/REPL.c
@@ -89,11 +89,6 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Page *page) {
 		free(page); 
 		exit(EXIT_SUCCESS);	
 	} 
-	else if(strcmp(input_buffer->buffer, ".btree") == 0) {
-		printf("Tree:\n");
-		print_tree(table->pager, 0, 0);
-		return META_COMMAND_SUCCESS; 
-	}
 	else {
 		return META_COMMAND_UNRECOGNIZED_COMMAND;	
 	}
