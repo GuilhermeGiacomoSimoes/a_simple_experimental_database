6c017e0ca76023477801f412a4a22581714a0b6c
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-03-22 22:00:05 -0300

    style: remove unnecessary space

--- src/REPL.c
+++ src/REPL.c
@@ -166,7 +166,7 @@ ExecuteResult execute_select(Table* table) {
 	Cursor* cursor = table_start(table);
 
 	Row row;
-	while( ! ( cursor->end_of_table ) ){
+	while(!(cursor->end_of_table)){
 		deserialize_row(cursor_value(cursor), &row);
 		print_row(&row);
 		cursor_advance(cursor);
