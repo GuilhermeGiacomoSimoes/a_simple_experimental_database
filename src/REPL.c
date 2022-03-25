e088d900971839df0f50abcc79fd7a16dbc36e54
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-03-24 22:47:45 -0300

    refactor: execute_select() it will be refactor before

--- src/REPL.c
+++ src/REPL.c
@@ -115,18 +115,7 @@ ExecuteResult execute_insert(Statement* statement, Page *root) {
 	return EXECUTE_SUCCESS;
 }
 
-ExecuteResult execute_select(Table* table) {
-	Cursor* cursor = table_start(table);
-
-	Row row;
-	while(!(cursor->end_of_table)){
-		deserialize_row(cursor_value(cursor), &row);
-		print_row(&row);
-		cursor_advance(cursor);
-	}
-
-	free(cursor);
-
+ExecuteResult execute_select() {
 	return EXECUTE_SUCCESS;
 }
 
