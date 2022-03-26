9b853021f116bd4b75a899b3dbe8058775b209c8
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-03-26 11:13:42 -0300

    style: else is unnecessary

--- src/REPL.c
+++ src/REPL.c
@@ -55,9 +55,7 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Page *page) {
 		free(page); 
 		exit(EXIT_SUCCESS);	
 	} 
-	else {
-		return META_COMMAND_UNRECOGNIZED_COMMAND;	
-	}
+	return META_COMMAND_UNRECOGNIZED_COMMAND;	
 }
 
 PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
