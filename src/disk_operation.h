15e63cd5eff24ed9e616412db3cd2b0032add5c9
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-03-26 08:44:35 -0300

    refactor: moved a constants declation to a header file

--- src/disk_operation.h
+++ src/disk_operation.h
@@ -1,6 +1,14 @@
 #include "structure.h"
 
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 #define NAME_FILE_DATABASE "database.db";
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0; 
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE; 
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE; 
 
 void disk_write(Page* page);
 Page* disk_read(Page* page, int number_child);
