ba75494da84fae62d8b22709785bb200f80b2e1d
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2022-04-05 21:01:01 -0300

    fix: 32 bits is very short

--- src/b_tree.h
+++ src/b_tree.h
@@ -1,7 +1,7 @@
 #include "structure.h"
 #include "disk_operation.h"
 
-Row* b_tree_search(Page *page, uint32_t wanted_element);
+Row* b_tree_search(Page *page, int wanted_element);
 void b_tree_insert(Page *root, Row *k);
 Page* load_root();
 void b_tree_insert_nonfull(Page *page, Row* k);
