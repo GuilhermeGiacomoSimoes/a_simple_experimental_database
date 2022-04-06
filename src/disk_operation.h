cee3fde87b54478aefd42514ca94c2c9f03dd849
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2022-04-05 21:01:29 -0300

    fix: creating a function to return atribute size

--- src/disk_operation.h
+++ src/disk_operation.h
@@ -1,8 +1,9 @@
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 #define NAME_FILE_DATABASE "database.db";
 
-const uint32_t ID_SIZE = sizeof(Row->id);
-const uint32_t USERNAME_SIZE = sizeof(Row->username);
-const uint32_t EMAIL_SIZE = sizeof(Row->email);
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
 const uint32_t ID_OFFSET = 0; 
 const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE; 
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE; 
