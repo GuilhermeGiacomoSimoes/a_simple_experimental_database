2f299b514496f9de04927dbb21e68e6fd8e44336
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2022-04-06 22:02:10 -0300

    fix: a const in time-compile

--- src/disk_operation.h
+++ src/disk_operation.h
@@ -1,12 +1,11 @@
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 #define NAME_FILE_DATABASE "database.db"
-
-const uint32_t ID_SIZE = size_of_attribute(Row, id);
-const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
-const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
-const uint32_t ID_OFFSET = 0; 
-const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE; 
-const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE; 
+#define ID_SIZE size_of_attribute(Row, id);
+#define USERNAME_SIZE size_of_attribute(Row, username);
+#define EMAIL_SIZE size_of_attribute(Row, email);
+#define ID_OFFSET 0; 
+#define USERNAME_OFFSET ID_OFFSET + ID_SIZE; 
+#define EMAIL_OFFSET USERNAME_OFFSET + USERNAME_SIZE; 
 
 void disk_write(Page* page);
 Page* disk_read(Page* page, int number_child);
