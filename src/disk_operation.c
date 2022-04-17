46cb91a70ebcc97b0ec01ea468ff21e1792284c0
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2022-04-17 17:45:49 -0300

    import structure file header

--- src/disk_operation.c
+++ src/disk_operation.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdint.h>
-#include "disk_operation.h"
+#include "structure.h"
 
 void serialize_row(Row* source, void* destination) {
 	memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
