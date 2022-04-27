ff600dbdc86cb56177d38b94d71b74e27b8d697c
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-04-26 22:53:48 -0300

    style: change to tab

--- src/disk_operation.c
+++ src/disk_operation.c
@@ -27,15 +27,15 @@
 #define ADDRESS_MEMMORY_OFFSET ELEMS_OFFSET + ADDRESS_MEMMORY_SIZE 
 
 void static serialize_row(Row* source, void* destination) {
-  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
-  memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+  	memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  	memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+  	memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
 }
 
 void static deserialize_row(void* source, Row* destination) {
-  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
-  memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
-  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+  	memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  	memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+  	memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
 void static serialize(Page* source, void* destination) {
