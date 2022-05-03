dee43498d1b987d3f9f6c7c8d5da7f92e8324167
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-05-02 22:36:05 -0300

    fix: fix the position pointer into fd

--- src/disk_operation.c
+++ src/disk_operation.c
@@ -75,9 +75,9 @@ void disk_write(Page* page) {
 		exit(EXIT_FAILURE);
 	}
 
-	off_t offset = lseek(fd, page->current_address_memmory, SEEK_END);
+	off_t offset = lseek(fd, page->current_address_memmory, SEEK_SET);
 	if(!page->current_address_memmory) {
-		offset = lseek(fd, 0, SEEK_SET);
+		offset = lseek(fd, 0, SEEK_END);
 		page->current_address_memmory = offset;
 	}
 
