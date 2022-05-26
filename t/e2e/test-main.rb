6e0f25a2d10c280e96b9da376b1418b3cd643ace
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-05-25 22:37:58 -0300

    initial tests e2e

---
+++ t/e2e/test-main.rb
@@ -0,0 +1,30 @@
+describe 'database' do 
+  def run_script(commands)
+    raw_output = nil
+    IO.popen('./database.db', "r+") do |pipe|
+      commands.each do |command|
+        pipe.puts command
+      end
+
+      pipe.close_write
+
+      raw_output = pipe.gets(nil)
+    end
+    raw_output.split("\n")
+  end
+
+
+
+  it 'inserts and retrieves a row' do 
+    result = run_script([
+      "insert 1 guilherme", 
+      "select 1"
+      ".exit"
+    ])
+    expect(result).to match_array([
+      "db> execute.",
+      "db> (1, guilherme)", 
+      "db>"
+    ])
+  end
+end
