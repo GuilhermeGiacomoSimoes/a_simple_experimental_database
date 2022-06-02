8d75561129410dcff8a7e3ea7d783177157adf89
Guilherme Giácomo Simões <trintaeoitogc@gmail.com>
2022-06-01 22:47:14 -0300

    tests: implementado tests de metacommend

--- t/unit-tests/main.cpp
+++ t/unit-tests/main.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
+#include "main.c";
+
+int TEST_do_meta_command_exit() {
+	InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
+	input_buffer->bugger = ".exit";
+	Page *page = malloc(page);
+
+	MetaCommandResult meta_command_result = do_meta_command(input_buffer, page);
+
+	return meta_command_result != META_COMMAND_UNRECOGNIZED_COMMAND;
+}
 
 int main() {
-	return 0;
+	return TEST_do_meta_command_exit();
 }
