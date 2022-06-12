aef16ccdf66bba249003e3c6d242fa0c39bb961b
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2022-06-12 10:58:13 -0300

    create a file for the call in file tests

---
+++ t/unit-tests/test-main.hh
@@ -0,0 +1,28 @@
+#define ALPH_ASSERT_THROW(condition) \
+{									\
+	if(!(condition)) {\
+		throw std::runtime_error( std::string(__FILE__) \
+				+ std::string(":")\
+				+ std::to_string(__LINE__)\
+				+ std::string(" in ")\
+				+ std::string(__PRETTY_FUNCTION__)\
+				);\
+	}\
+}\
+
+
+#define ALPH_ASSERT_EQUAL(x, y) \
+{									\
+	if((x) != (y)) {\
+		throw std::runtime_error( std::string(__FILE__) \
+				+ std::string(":")\
+				+ std::to_string(__LINE__)\
+				+ std::string(" in ")\
+				+ std::string(__PRETTY_FUNCTION__)\
+				+ std::string(": ")\
+				+ std::to_string((x))\
+				+ std::string(" != ")\
+				+ std::to_string((y))\
+				);\
+	}\
+}\
