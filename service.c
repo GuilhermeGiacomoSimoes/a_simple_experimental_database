9bae9c497136391063bbd6971bfe9ccb75883586
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2022-12-08 23:16:38 -0300

    feat: create functions of service

--- service.c
+++ service.c
@@ -1,5 +1,26 @@
+#include<stdlib.h>
+#include<stdint.h>
+#include<string.h>
+
 #include "service.h"
 
-Result execute(Input_Buffer* input_buffer) {
+Result do_meta_command(Input_Buffer* input_buffer) {
+	if(strcmp(input_buffer->buffer, ".exit") == 0){
+		exit(EXIT_SUCCESS);	
+	}
 
+	Result *result = (Result*) malloc(sizeof(Result));
+	result->description = "ERROR: meta command does not exist\n";
+	result->code = 0;
+	return *(result);
+}
+
+static uint8_t is_meta_command(Input_Buffer* input_buffer) {
+	return input_buffer->buffer[0] == '.';
+}
+
+Result execute(Input_Buffer* input_buffer) {
+	if(is_meta_command(input_buffer)) {
+		return do_meta_command(input_buffer);
+	}		
 }
