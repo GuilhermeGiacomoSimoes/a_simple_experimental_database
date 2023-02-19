c74765a82ba91ed32cbb9ffdf82c970d4ed34ffe
simoes <trintaeoitogc@gmail.com>
2023-02-19 19:08:28 -0300

    refactor: change name of the atribute

--- b_tree.c
+++ b_tree.c
@@ -8,10 +8,10 @@
 
 Row* search(Page *page, int wanted_element) {
 	uint32_t i = 0;
-	while (i <= page->elems && (page->info[i] != NULL && wanted_element > page->info[i]->id)) {
+	while (i <= page->amount_of_keys && (page->info[i] != NULL && wanted_element > page->info[i]->id)) {
 		i++;	
 	}
-	if(i <= page->elems && wanted_element == page->info[i]->id) {
+	if(i <= page->amount_of_keys && wanted_element == page->info[i]->id) {
 		return page->info[i];
 	}
 	else if(page->leaf) {
@@ -31,7 +31,7 @@ Page* load_root() {
 Page* create() {
 	Page *x = malloc(sizeof(Page));
 	x->leaf = 1;
-	x->elems = 0;
+	x->amount_of_keys = 0;
 	disk_write(x);
 	return x;
 }
@@ -42,7 +42,7 @@ void static b_tree_split_child(Page *parent_not_full, uint32_t index_child_full)
 	Page *child_full = parent_not_full->childs[index_child_full];
 
 	page_new->leaf = child_full->leaf;
-	page_new->elems = t - 1;
+	page_new->amount_of_keys = t - 1;
 
 	for(int j = 1; j <= t - 1; j++) {
 		page_new->info[j] = child_full->info[j + t];
@@ -54,19 +54,19 @@ void static b_tree_split_child(Page *parent_not_full, uint32_t index_child_full)
 		}
 	}
 
-	child_full->elems = t - 1;
-	for(int j = parent_not_full->elems + 1; j < index_child_full + 1; j++) {
+	child_full->amount_of_keys = t - 1;
+	for(int j = parent_not_full->amount_of_keys + 1; j < index_child_full + 1; j++) {
 		parent_not_full->childs[j+1] = parent_not_full->childs[j];
 	}
 
 	parent_not_full->childs[index_child_full+1] = page_new;
 
-	for(int j = parent_not_full->elems; j < index_child_full + 1; j++) {
+	for(int j = parent_not_full->amount_of_keys; j < index_child_full + 1; j++) {
 		parent_not_full->childs[j+1] = parent_not_full->childs[j];
 	}
 
-	parent_not_full->info[index_child_full] = child_full->info[parent_not_full->elems];
-	parent_not_full->elems ++;
+	parent_not_full->info[index_child_full] = child_full->info[parent_not_full->amount_of_keys];
+	parent_not_full->amount_of_keys ++;
 
 	disk_write(child_full);
 	disk_write(page_new);
@@ -74,7 +74,7 @@ void static b_tree_split_child(Page *parent_not_full, uint32_t index_child_full)
 }
 
 void static b_tree_insert_nonfull(Page *page, Row* k) {
-	int i = page->elems - 1;
+	int i = page->amount_of_keys - 1;
 
 	if(page->leaf) {
 		while(i >= 0 && k->id < page->info[i]->id) {
@@ -82,7 +82,7 @@ void static b_tree_insert_nonfull(Page *page, Row* k) {
 			i --;
 		}
 		page->info[i+1] = k;
-		page->elems ++;
+		page->amount_of_keys ++;
 		disk_write(page);
 		free(page); 
 	}
@@ -95,7 +95,7 @@ void static b_tree_insert_nonfull(Page *page, Row* k) {
 		Page *child = malloc(sizeof(Page));
 		child = disk_read(page, i);  
 
-		if(child->elems == MAX_ELEMENTS) {
+		if(child->amount_of_keys == MAX_ELEMENTS) {
 			b_tree_split_child(page, i);
 			if(k->id > page->info[i]->id) {
 				i ++;
@@ -106,11 +106,11 @@ void static b_tree_insert_nonfull(Page *page, Row* k) {
 }
 
 int insert(Page *root, Row *k) {
-	if(root->elems == MAX_ELEMENTS) {
+	if(root->amount_of_keys == MAX_ELEMENTS) {
 		Page *s = malloc(sizeof(Page))	;
 		root = s;
 		s->leaf = 0;
-		s->elems = 0;
+		s->amount_of_keys = 0;
 		s->childs[0] = root;
 		b_tree_split_child(s, 1);
 		b_tree_insert_nonfull(s, k);
