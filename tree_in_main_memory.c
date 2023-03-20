8698623c693dc27d9e48f47f75f6c64747b9192e
simoes <trintaeoitogc@gmail.com>
2023-03-20 08:42:45 -0300

    refactor: rename file

--- tree_in_main_memory.c
+++ tree_in_main_memory.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdint.h>
+#include<string.h>
+#include<stdlib.h>
 
 #include "structure.h"
 #include "tree.h"
@@ -16,7 +18,7 @@ Row* search(Page *page, int wanted_element) {
         if(*(integer_array_for_test_mock + index) == wanted_element) {
             char *value = *(values_array_for_test_mock + index);
             Row *row = malloc(sizeof(Row));
-            row->data = value;
+	        strcpy(row->data, value);
             row->id = index;
             return row;
         }
@@ -37,8 +39,8 @@ Page* create() {
 int insert(Page *root, Row *k) {
     for(uint32_t index = 0; index < MAX_SIZE_ARR; index++) {
         if(*(integer_array_for_test_mock + index) == NULL) {
-            *(integer_array_for_test_mock + index) = ++index;
             values_array_for_test_mock[index] = k->data;
+            *(integer_array_for_test_mock + index) = ++index;
             return index;
         }
     }
