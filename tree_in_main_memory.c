ea26b888d0d7dafee0ea1e858b3e00ace4b983c8
simoes <trintaeoitogc@gmail.com>
2023-03-22 16:56:41 -0300

    feat: received arrays in args function

--- tree_in_main_memory.c
+++ tree_in_main_memory.c
@@ -10,10 +10,13 @@
 
 #define MAX_SIZE_ARR 256
 
-uint32_t integer_array_for_test_mock[MAX_SIZE_ARR];
-char *values_array_for_test_mock[MAX_SIZE_ARR];
+Row* search(Page *page, int wanted_element, ...) {
+    va_list valist;
+    va_start(valist, 2);
 
-Row* search(Page *page, int wanted_element) {
+    uint32_t *integer_array_for_test_mock = va_arg(valist, uint32_t*);
+    char** values_array_for_test_mock = va_arg(valist, char**);
+
     Row *row = (Row*) malloc(sizeof(Row));
     row->id = 0;
 
@@ -26,6 +29,7 @@ Row* search(Page *page, int wanted_element) {
         }
     }
 
+    va_end(valist);
     return row;
 }
 
@@ -42,7 +46,13 @@ Page* create() {
     return NULL;
 }
 
-int insert(Page *root, Row *k) {
+int insert(Page *root, Row *k, ...) {
+    va_list valist;
+    va_start(valist, 2);
+
+    uint32_t *integer_array_for_test_mock = va_arg(valist, uint32_t*);
+    char** values_array_for_test_mock = va_arg(valist, char**);
+
     for(uint32_t index = 0; index < MAX_SIZE_ARR; index++) {
         if(*(integer_array_for_test_mock + index) == 0) {
             values_array_for_test_mock[index] = k->data;
