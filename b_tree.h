d4d3d45f6a3961ce1b03a0c94c3c3624f586e428
GuilhermeGiacomoSimoes <trintaeoitogc@gmail.com>
2023-12-30 16:38:33 -0300

    refactor: remove uneccessary files

--- b_tree.h
+++
@@ -1,9 +0,0 @@
-#ifndef _B_TREE_H_
-#define _B_TREE_H_
-
-Row* b_tree_search(Page *page, int wanted_element);
-Page* load_root();
-void static b_tree_insert_nonfull(Page *page, Row* k);
-void b_tree_insert(Page *root, Row *k);
-
-#endif
